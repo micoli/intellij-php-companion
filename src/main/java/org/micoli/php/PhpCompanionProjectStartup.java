e7dbb6ee258a6ac5c7714043fa5c0585ae269251
Olivier MICHAUD <olivier.michaud@yousign.com>
2025-07-12 18:24:28 +0200

    chore: Fix deprecation on project startup

--- src/main/java/org/micoli/php/PhpCompanionProjectStartup.java
+++ src/main/java/org/micoli/php/PhpCompanionProjectStartup.java
@@ -2,16 +2,19 @@ package org.micoli.php;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.startup.ProjectActivity;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class PhpCompanionProjectStartup implements StartupActivity {
+public class PhpCompanionProjectStartup implements ProjectActivity {
     private static final Logger LOG = Logger.getInstance(PhpCompanionProjectStartup.class);
 
     @Override
-    public void runActivity(@NotNull Project project) {
+    public @Nullable Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
         LOG.info("Starting PhpCompanionProjectService for project: " + project.getName());
-
-        PhpCompanionProjectService service = PhpCompanionProjectService.getInstance(project);
+        PhpCompanionProjectService.getInstance(project);
+        return Unit.INSTANCE;
     }
 }
